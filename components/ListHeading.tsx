8a7013bd79dffd019cc88dd9809b5abcd69204f4
RomanBliashuk <rblyashuk@getrevscale.com>
2026-05-25 16:04:59 +0300

    implemented home screen ui

---
+++ components/ListHeading.tsx
@@ -0,0 +1,19 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+interface ListHeadingProps {
+  title: string;
+}
+
+const ListHeading = ({ title }: ListHeadingProps) => {
+  return (
+    <View className="list-head">
+      <Text className="list-title">{title}</Text>
+
+      <TouchableOpacity className="list-action">
+        <Text className="list-action-text">View all</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ListHeading;
---
+++ components/UpcomingSubscriptionCard.tsx
@@ -0,0 +1,31 @@
+import { formatCurrency } from "@/lib/utils";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+interface UpcomingSubscriptionCardProps {
+  data: UpcomingSubscription;
+}
+
+const UpcomingSubscriptionCard = ({
+  data: { icon, name, price, currency, daysLeft },
+}: UpcomingSubscriptionCardProps) => {
+  return (
+    <View className="upcoming-card">
+      <View className="upcoming-row">
+        <Image source={icon} className="upcoming-icon" />
+        <View>
+          <Text className="upcoming-price">
+            {formatCurrency(price, currency)}
+          </Text>
+          <Text className="upcoming-meta" numberOfLines={1}>
+            {daysLeft > 1 ? `${daysLeft} days left` : "Last day"}
+          </Text>
+        </View>
+      </View>
+
+      <Text className="upcoming-name">{name}</Text>
+    </View>
+  );
+};
+
+export default UpcomingSubscriptionCard;
